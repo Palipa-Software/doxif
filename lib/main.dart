0371f175183d3cea8fe708b0d463a3a43ae1a52f
hasanoz07 <hasanozz2002@gmail.com>
2023-02-11 00:26:57 +0300

    add pack

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:sizer/sizer.dart';
 
 import 'package:tutorai/routes/routes.dart';
 
@@ -12,15 +12,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ResponsiveSizer(
-      builder: (context, orientation, screenType) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
         return GetMaterialApp(
-          title: 'BoilerPlate',
+          title: 'SeramCepte',
           debugShowCheckedModeBanner: false,
           initialRoute: AppPages.INITIAL,
           getPages: AppPages.routes,
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.green,
             appBarTheme: const AppBarTheme(
               backgroundColor: Colors.transparent,
               elevation: 0,
