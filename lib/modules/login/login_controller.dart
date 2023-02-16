523501a76b8fa67d61d5a4a561d228357662efdf
hasanoz07 <hasanozz2002@gmail.com>
2023-02-16 15:57:36 +0300

    login part-5 register part-4

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tutorai/routes/routes.dart';
 import 'package:tutorai/shared/constants/constants.dart';
 
@@ -8,6 +10,34 @@ class LoginController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      // Once signed in, return the UserCredential
+      var user = await FirebaseAuth.instance.signInWithCredential(credential);
+      await _firestore.collection("users").doc(user.user?.uid).set({
+        "name": googleUser.displayName,
+        "surname": "",
+        "phone": "",
+        "email": googleUser.email,
+      });
+      return user;
+    } on FirebaseAuthException catch (e) {
+      Get.snackbar("Error", e.message.toString());
+    }
+    return null;
+  }
+
   Future signIn(String email, String password, BuildContext context) async {
     showDialog(
       context: context,
