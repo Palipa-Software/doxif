1e223c364583aa254bb549265d5b1d9b3da89e26
Harun <humann210@gmail.com>
2023-03-05 03:58:43 +0300

    warning screen bug fixed

--- lib/modules/warning/warning_controller.dart
+++ lib/modules/warning/warning_controller.dart
@@ -16,9 +16,13 @@ class WarningController extends GetxController {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<void> getSensorID() async {
-    var warnings = FirebaseFirestore.instance.collection("warnings");
-    var warning = await warnings.get();
-    var element = warning.docs;
+    var tempWarnings = FirebaseFirestore.instance.collection("tempWarnings");
+    var tempWarning = await tempWarnings.get();
+    var tempWarningElement = tempWarning.docs;
+    var humidityWarnings =
+        FirebaseFirestore.instance.collection("humidityWarnings");
+    var humidityWarning = await humidityWarnings.get();
+    var humidityWarningElement = humidityWarning.docs;
 
     var sensor = FirebaseFirestore.instance
         .collection("allRegions")
@@ -34,93 +38,88 @@ class WarningController extends GetxController {
 
       var temp = await databaseRef.child("sicaklik").get();
       var humidity = await databaseRef.child("nem").get();
-      // realTimeDatas(databaseRef);
-      if (temp.value == null || humidity.value == null) {
-        Get.snackbar("Title", "Message",
-            backgroundColor: AppColors.appColor.withOpacity(.8),
-            titleText: AutoSizeText(
-              regions[i]["regionName"] + " " + "serası",
-              style: TextStyle(
-                fontFamily: "Rubik Bold",
-                color: AppColors.white,
-                fontSize: 13.sp,
+      for (var index = 0; index < humidityWarningElement.length; index++) {
+        if (temp.value == null || humidity.value == null) {
+          Get.snackbar("Title", "Message",
+              backgroundColor: AppColors.appColor.withOpacity(.8),
+              titleText: AutoSizeText(
+                regions[i]["regionName"] + " " + "serası",
+                style: TextStyle(
+                  fontFamily: "Rubik Bold",
+                  color: AppColors.white,
+                  fontSize: 13.sp,
+                ),
               ),
-            ),
-            messageText: Text(
-              AppStrings.defectiveSensor,
-              style: TextStyle(
-                fontFamily: "Rubik Regular",
-                color: AppColors.white,
-                fontSize: 10.sp,
-              ),
-            ));
-      } else if (temp.value.toString() == "nan") {
-        Get.snackbar("Title", "Message",
-            backgroundColor: AppColors.appColor.withOpacity(.8),
-            titleText: AutoSizeText(
-              regions[i]["regionName"] + " " + "serası",
-              style: TextStyle(
-                fontFamily: "Rubik Bold",
-                color: AppColors.white,
-                fontSize: 13.sp,
-              ),
-            ),
-            messageText: Text(
-              AppStrings.defectiveSensor,
-              style: TextStyle(
-                fontFamily: "Rubik Regular",
-                color: AppColors.white,
-                fontSize: 10.sp,
-              ),
-            ));
-        print("Hata");
-      } else if (double.tryParse(humidity.value.toString())!.toDouble() >
-          double.tryParse(element[i]["val"])!) {
-        print(humidity.value.toString());
-        print(double.tryParse(element[i]["val"])!);
-        warningList.add({
-          "title": regions[i]["regionName"],
-          "subtitle": element[i]["desc"],
-          "ic_path": element[i]["icon"],
-        });
-        print("Yüksek Nem");
-      } else if (double.tryParse(temp.value.toString())!.toDouble() >
-          double.tryParse(element[i]["val"])!) {
-        print(humidity.value.toString());
+              messageText: Text(
+                AppStrings.defectiveSensor,
+                style: TextStyle(
+                  fontFamily: "Rubik Regular",
+                  color: AppColors.white,
+                  fontSize: 10.sp,
+                ),
+              ));
+          break;
+        } else if (double.tryParse(humidity.value.toString())!.toDouble() >
+            double.tryParse(humidityWarningElement[0]["val"])!) {
+          print(humidity.value.toString());
+          print(double.tryParse(humidityWarningElement[0]["val"])!);
+          warningList.add({
+            "title": regions[i]["regionName"],
+            "subtitle": humidityWarningElement[0]["desc"],
+            "ic_path": humidityWarningElement[0]["icon"],
+          });
+          print("Yüksek Nem");
+          break;
+        } else if (double.tryParse(humidity.value.toString())!.toDouble() <
+            double.tryParse(humidityWarningElement[1]["val"])!) {
+          print(humidity.value.toString());
 
-        print(double.tryParse(element[i]["val"])!);
-        warningList.add({
-          "title": regions[i]["regionName"],
-          "subtitle": element[i]["desc"],
-          "ic_path": element[i]["icon"],
-        });
+          print(double.tryParse(humidityWarningElement[1]["val"])!);
+          warningList.add({
+            "title": regions[i]["regionName"],
+            "subtitle": humidityWarningElement[1]["desc"],
+            "ic_path": humidityWarningElement[1]["icon"],
+          });
 
-        print("Yüksek Sıcaklık");
-      } else if (double.tryParse(humidity.value.toString())!.toDouble() <
-          double.tryParse(element[i]["val"])!) {
-        print(humidity.value.toString());
-
-        print(double.tryParse(element[i]["val"])!);
-        warningList.add({
-          "title": regions[i]["regionName"],
-          "subtitle": element[i]["desc"],
-          "ic_path": element[i]["icon"],
-        });
-
-        print("Düşük nem");
-      } else if (double.tryParse(temp.value.toString())!.toDouble() <
-          double.tryParse(element[i]["val"])!) {
-        print(humidity.value.toString());
-
-        print(double.tryParse(element[i]["val"])!);
-        warningList.add({
-          "title": regions[i]["regionName"],
-          "subtitle": element[i]["desc"],
-          "ic_path": element[i]["icon"],
-        });
-
-        print("Düşük sıcaklık");
+          print("Düşük nem");
+          break;
+        }
       }
+      for (var tempIndex = 0;
+          tempIndex < tempWarningElement.length;
+          tempIndex++) {
+        if (temp.value == null || humidity.value == null) {
+          break;
+        } else if (double.tryParse(temp.value.toString())!.toDouble() >
+            double.tryParse(tempWarningElement[0]["val"])!) {
+          print(temp.value.toString());
+
+          print(double.tryParse(tempWarningElement[tempIndex]["val"])!);
+          warningList.add({
+            "title": regions[i]["regionName"],
+            "subtitle": tempWarningElement[0]["desc"],
+            "ic_path": tempWarningElement[0]["icon"],
+          });
+
+          print("Yüksek Sıcaklık");
+          break;
+        } else if (double.tryParse(temp.value.toString())!.toDouble() <
+            double.tryParse(tempWarningElement[1]["val"])!) {
+          print(temp.value.toString());
+
+          print(double.tryParse(tempWarningElement[1]["val"])!);
+          warningList.add({
+            "title": regions[i]["regionName"],
+            "subtitle": tempWarningElement[1]["desc"],
+            "ic_path": tempWarningElement[1]["icon"],
+          });
+
+          print("Düşük sıcaklık");
+          break;
+        }
+      }
+
+      // realTimeDatas(databaseRef);
     }
   }
 
